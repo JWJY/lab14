b1578d3a69713adb8ce78fbd417164217fd22cbc
JWJY <warisara.jy@gmail.com>
2023-01-13 17:12:26 +0700

    completed

--- lab14_2.cpp
+++ lab14_2.cpp
@@ -21,3 +21,30 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+
+void updateImage(bool A[][M],int s,int x,int y){
+    for(int i=0 ; i<30 ; i++){
+        for(int j=0 ; j<70 ; j++){
+            if(sqrt(pow(i-x,2)+pow(j-y,2)) <= s-1){
+                A[i][j] = 1;
+            } 
+        }
+    }
+}
+
+
+void showImage(const bool B[][M]){
+    cout << "------------------------------------------------------------------------\n";
+    for(int i=0 ; i<N ; i++){
+        cout << "|";
+        for(int j=0 ; j<M ; j++){
+            if(B[i][j] == 1) cout << "*";
+            else{
+                cout << " ";
+            }
+        }
+        cout << "|";
+        cout << "\n";
+    }
+    cout << "------------------------------------------------------------------------\n";
+}
