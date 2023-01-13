9294a108c572c07e02b628d8e5e4bcae09ed9767
JWJY <warisara.jy@gmail.com>
2023-01-13 15:57:49 +0700

    completed

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -17,3 +17,26 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double a[], int n, double b[]){
+    double max = a[0], min = a[0];
+    double sum = 0, sum_SD = 0, mean, SD, sum_GM = 1, GM, sum_HM = 0, HM;
+    for(int i=0 ; i<n ; i++){
+        sum += a[i];
+        sum_SD += pow(a[i],2);
+        sum_GM *= a[i];
+        sum_HM += (1/a[i]);
+        if(a[i]>max) max = a[i];
+        if(a[i]<min) min = a[i];
+    }
+    mean = (sum/n);
+    SD = sqrt((sum_SD/n) - pow(mean,2));
+    GM = pow(sum_GM,1.0/n);
+    HM = (n/sum_HM);
+    b[0] = mean;
+    b[1] = SD;
+    b[2] = GM;
+    b[3] = HM;
+    b[4] = max;
+    b[5] = min;
+}
